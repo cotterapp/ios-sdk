a1d8217959ccae45f6d60a03d142a38bcebf11e0
rayandrie <andrieja@usc.edu>
2020-02-01 22:50:05 -0800

    confirm added pin verification functionality

--- Source/Custom UI Fields/OneTimeCodeTextField.swift
+++ Source/Custom UI Fields/OneTimeCodeTextField.swift
@@ -10,6 +10,7 @@ import Foundation
 class OneTimeCodeTextField : UITextField {
     private var isConfigured = false
     private var defaultCharacter = "•"
+    private var isPinVisible = false
     private var digitLabels = [UILabel]()
     private lazy var tapRecognizer: UITapGestureRecognizer = {
         let recognizer = UITapGestureRecognizer()
@@ -19,7 +20,9 @@ class OneTimeCodeTextField : UITextField {
     
     var didEnterLastDigit: ((String) -> Void)? // Function to run when last PIN digit has been entered
     
-    func configure(with slotCount: Int = 6) {
+    var removeErrorMsg: (() -> Void)? // Function to run when backspace is entered. This is to determine whether to remove error msg
+    
+    public func configure(with slotCount: Int = 6) {
         guard isConfigured == false else { return }
         isConfigured.toggle()
         
@@ -48,7 +51,7 @@ class OneTimeCodeTextField : UITextField {
         delegate = self
     }
     
-    // Create the Password Labels 
+    // Create the initial Password Labels
     private func createLabelsStackView(with count: Int) -> UIStackView {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -57,7 +60,7 @@ class OneTimeCodeTextField : UITextField {
         stackView.distribution = .fillEqually
         stackView.spacing = 8
         
-        // Adding each password input label
+        // Adding each initial password input label
         for _ in 1...count {
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
@@ -65,6 +68,7 @@ class OneTimeCodeTextField : UITextField {
             label.font = .systemFont(ofSize: 40)
             label.isUserInteractionEnabled = true
             label.text = defaultCharacter
+            label.textColor = UIColor.lightGray
             
             stackView.addArrangedSubview(label)
             
@@ -74,22 +78,34 @@ class OneTimeCodeTextField : UITextField {
         return stackView
     }
     
+    // Toggle PIN Visibility between dots or numbers
+    public func togglePinVisibility() {
+        isPinVisible.toggle()
+        codeDidChange()
+    }
+    
     // On Code Change (I/O) Function
-    @objc private func codeDidChange() {
+    @objc
+    private func codeDidChange() {
         // Ensure only 6 digit input
         guard let text = self.text, text.count <= digitLabels.count else { return }
         
         // For each label, show the dot/number on the text field
         for i in 0 ..< digitLabels.count {
             let currentLabel = digitLabels[i]
+            currentLabel.text = defaultCharacter
             
             if i < text.count {
-                // User has inputted text, so show in text field label
+                // User has inputted text here, so show in text field label
                 let index = text.index(text.startIndex, offsetBy: i)
-                currentLabel.text = String(text[index])
+                if isPinVisible {
+                    // Show the numbers
+                    currentLabel.text = String(text[index])
+                }
+                currentLabel.textColor = UIColor.black
             } else {
-                // User is backspacing, so just set it to default char
-                currentLabel.text = defaultCharacter
+                // User has not inputted text here, so it remains default char
+                currentLabel.textColor = UIColor.lightGray
             }
         }
         
@@ -105,6 +121,14 @@ extension OneTimeCodeTextField : UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let characterCount = textField.text?.count else { return false } // characterCount is one less char than the one in the text field
         let backspaceEntered = string == ""
+        
+        // If User entered a backspace from the last char, and error message is shown, remove error messages
+        print("characterCount: ", characterCount)
+        print("digitLabels.count: ", digitLabels.count)
+        if characterCount == digitLabels.count && backspaceEntered {
+            removeErrorMsg?()
+        }
+        
         return characterCount < digitLabels.count || backspaceEntered
     }
 }
--- Source/PINViewController.swift
+++ Source/PINViewController.swift
@@ -8,11 +8,120 @@
 import Foundation
 import UIKit
 
-public class PINViewController: UIViewController {
+public class PINViewController : UIViewController {
+    let closeTitle = "Yakin tidak Mau Buat PIN Sekarang?"
+    let closeMessage = "PIN Ini diperlukan untuk keamanan akunmu, lho."
+    let stayText = "Input PIN"
+    let leaveText = "Lain Kali"
+    
+    // Code Text Field
+    @IBOutlet weak var codeTextField: OneTimeCodeTextField!
+    
+    // PIN Visibility Toggle Button
+    @IBOutlet weak var pinVisibilityButton: UIButton!
+    let showPinText = "Lihat PIN"
+    let hidePinText = "Sembunyikan"
+    
+    // Error Label
+    @IBOutlet weak var errorLabel: UILabel!
+    var showErrorMsg = false
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("loaded PIN Cotter View")
+        
+        // Implement Custom Back Button instead of default in Nav controller
+        self.navigationItem.hidesBackButton = true
+        let crossButton = UIBarButtonItem(title: "\u{2717}", style: UIBarButtonItem.Style.plain, target: self, action: #selector(PINViewController.promptClose(sender:)))
+        crossButton.tintColor = UIColor.black
+        self.navigationItem.leftBarButtonItem = crossButton
+        
+        // Configure PIN Visibility Button
+        configurePinVisButton()
+        
+        // Add Password Code Text Field
+        codeTextField.configure()
+        
+        // Configure Error Msg Label
+        configureErrorMsg()
+        
+        // Instantiate Function to run when user enters wrong PIN code
+        codeTextField.removeErrorMsg = {
+            // Remove error msg if it is present
+            if self.showErrorMsg {
+                self.toggleErrorMsg()
+            }
+        }
+        
+        // Instantiate Function to run when PIN is fully entered
+        codeTextField.didEnterLastDigit = { code in
+            print("PIN Code Entered: ", code)
+            // Test: If code is 123456, show error. Else, is fine
+            if code == "123456" {
+                // Show errors, hide button
+                self.toggleErrorMsg()
+            }
+            
+            // TODO: Check for basic errors such as repeating digits and straight digits
+            
+            // TODO: Run API to check whether PIN is correct
+        }
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Open Keypad on View Appearance
+        codeTextField.becomeFirstResponder()
+    }
+    
+    private func configurePinVisButton() {
+        pinVisibilityButton.setTitleColor(UIColor(red: 0.0196, green: 0.4275, blue: 0, alpha: 1.0), for: .normal)
+        pinVisibilityButton.setTitle(showPinText, for: .normal)
+        pinVisibilityButton.isHidden = false
+    }
+    
+    private func configureErrorMsg() {
+        errorLabel.isHidden = true
+        errorLabel.textColor = UIColor(red: 0.8392, green: 0, blue: 0, alpha: 1.0)
+    }
+    
+    private func toggleErrorMsg() {
+        showErrorMsg.toggle()
+        errorLabel.isHidden.toggle()
+        pinVisibilityButton.isHidden.toggle()
+    }
+    
+    @IBAction func onClickPinVis(_ sender: UIButton) {
+        codeTextField.togglePinVisibility()
+        if sender.title(for: .normal) == showPinText {
+            sender.setTitle(hidePinText, for: .normal)
+        } else {
+            sender.setTitle(showPinText, for: .normal)
+        }
+    }
+    
+    @objc private func promptClose(sender: UIBarButtonItem) {
+        // Perform Prompt Alert
+        createAlert(title: closeTitle, message: closeMessage, stayText: stayText, leaveText: leaveText)
+    }
+    
+    // Alert Function for when User clicks close Pin Verification View
+    public func createAlert(title: String, message: String, stayText: String, leaveText: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: leaveText, style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: {
+                // Go back to previous screen
+                self.navigationController?.popViewController(animated: true)
+            })
+        }))
+        
+        alert.addAction(UIAlertAction(title: stayText, style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
 
     public override func didReceiveMemoryWarning() {
