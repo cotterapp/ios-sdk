7107ac8dbeeafddd7f862be60916eac7929fdd2e
Albert Putra Purnama <albertputrapurnama@gmail.com>
2020-02-06 02:15:11 +0700

    Move http request to CotterAPIService

--- Source/Service/CotterAPIService.swift
+++ Source/Service/CotterAPIService.swift
@@ -64,5 +64,55 @@ public class CotterAPIService {
     }()
     
     // TODO: http requests functions should be here
-    
+    public func http(method:String, path:String, data: [String:Any]?, succesCb: @escaping (String) -> Void, errCb: @escaping (String) -> Void) {
+        // set url path
+        let urlString = CotterAPIService.shared.getURL()!.absoluteString + path
+        let url = URL(string:urlString)!
+        
+        // create request
+        var request = URLRequest(url:url)
+        
+        // fill the required request headers
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(self.getSecretKey(), forHTTPHeaderField: "API_SECRET_KEY")
+        request.setValue(self.getKeyID(), forHTTPHeaderField: "API_KEY_ID")
+        request.httpMethod = method
+        
+        // fill in the body with json if exist
+        if data != nil {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: data!)
+        }
+        
+        // start http request
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data,
+                let response = response as? HTTPURLResponse,
+                error == nil else { // check for fundamental networking error
+                // TODO: error handling
+                print("error", error ?? "Unknown error")
+                return
+            }
+            
+            guard (200 ... 299) ~= response.statusCode else {   // check for http errors
+                print("statusCode should be 2xx, but is \(response.statusCode)")
+                print("response = \(response)")
+                
+                // error handling
+                DispatchQueue.main.async{
+                    errCb("statusCode should be 2xx, but is \(response.statusCode)")
+                }
+                
+                return
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+            
+            // if it reaches this point, that means the http request is successful
+            DispatchQueue.main.async{
+                succesCb(responseString!)
+            }
+        }
+        task.resume()
+    }
 }
